2306ebcccee32065e06cdf5b20c42744562844d1
Vint-Razor <vint-razor@yandex.ru>
2024-04-24 19:11:26 +0500

    5. Просмотр, редактирование и удаление вакансии [#504850 #492784]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -2,7 +2,6 @@ package ru.job4j.dreamjob.repository;
 
 import ru.job4j.dreamjob.model.Vacancy;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,12 +13,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new HashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "ИП \"ТАРАН\", требования: без опыта", LocalDateTime.now()));
-        save(new Vacancy(0, "Junior Java Developer", "ЧОП \"ВАРЯГ\", требования: 1 год опыта", LocalDateTime.now()));
-        save(new Vacancy(0, "Junior+ Java Developer", "Infinity Solution, требования: 2 года опыта", LocalDateTime.now()));
-        save(new Vacancy(0, "Middle Java Developer", "Теньков, требования: 2-3 года опыта", LocalDateTime.now()));
-        save(new Vacancy(0, "Middle+ Java Developer", "Сбер, требования: 3 года опыта", LocalDateTime.now()));
-        save(new Vacancy(0, "Senior Java Developer", "Яндекс, требования: 3-6 лет опыта", LocalDateTime.now()));
+        save(new Vacancy(0, "Intern Java Developer", "ИП \"ТАРАН\", требования: без опыта"));
+        save(new Vacancy(0, "Junior Java Developer", "ЧОП \"ВАРЯГ\", требования: 1 год опыта"));
+        save(new Vacancy(0, "Junior+ Java Developer", "Infinity Solution, требования: 2 года опыта"));
+        save(new Vacancy(0, "Middle Java Developer", "Теньков, требования: 2-3 года опыта"));
+        save(new Vacancy(0, "Middle+ Java Developer", "Сбер, требования: 3 года опыта"));
+        save(new Vacancy(0, "Senior Java Developer", "Яндекс, требования: 3-6 лет опыта"));
     }
 
     public static MemoryVacancyRepository getInstance() {
@@ -30,7 +29,6 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public Vacancy save(Vacancy vacancy) {
         vacancy.setId(nextId++);
-        vacancy.setCreationDate(LocalDateTime.now());
         vacancies.put(vacancy.getId(), vacancy);
         return vacancy;
     }
@@ -42,8 +40,8 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public boolean update(Vacancy vacancy) {
-        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> new Vacancy(oldVacancy.getId(),
-                vacancy.getTitle(), vacancy.getDescription(), oldVacancy.getCreationDate())) != null;
+        vacancy.setCreationDate(vacancies.get(vacancy.getId()).getCreationDate());
+        return vacancies.computeIfPresent(vacancy.getId(), (key, val) -> vacancy) != null;
     }
 
     @Override
