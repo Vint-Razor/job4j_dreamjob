c64e13fa1d89f92bdcca2eae9e24044ed503cd71
Vint-Razor <vint-razor@yandex.ru>
2024-04-30 15:10:25 +0500

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Vacancy;
-import ru.job4j.dreamjob.service.SimpleVacancyService;
 import ru.job4j.dreamjob.service.VacancyService;
 
 @Controller
--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -1,26 +1,32 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private Map<Integer, Candidate> candidates = new HashMap<>();
-    private int counterId = 1;
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final AtomicInteger counterId = new AtomicInteger(1);
 
     private MemoryCandidateRepository() {
         save(new Candidate(0, "Ivanov Ivan", "Java, Maven, GIT"));
         save(new Candidate(0, "Igor Yang", "Java, Maven, GIT, JUnit"));
-        save(new Candidate(0, "Roman Petrov", "Java, GIT, JOCOCO"));
+        save(new Candidate(0, "Roman Petrov", "Java, GIT, JACOCO"));
         save(new Candidate(0, "Andrey Volkov", "Java, C#, Python"));
         save(new Candidate(0, "Aleksey Tolstoy", "Java, Lisp, Basic"));
     }
 
     @Override
     public Candidate save(Candidate candidate) {
-        candidate.setId(counterId++);
+        candidate.setId(counterId.getAndIncrement());
         candidates.put(candidate.getId(), candidate);
         return candidate;
     }
